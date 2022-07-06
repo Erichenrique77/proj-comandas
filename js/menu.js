8a52a6e817af8dfcf9d7cb4383698380122276d7
erichenrique77 <erichenrique99911@gmail.com>
2022-07-06 11:42:47 -0300

    content

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.nav-link').click(function(e){
+        e.preventDefault()
+
+        let url = $(this).attr('href')
+
+        $('#content').empty()
+
+        $('#content').load(url)
+    })
+})
+
+//funçao que monitora os nav-links
